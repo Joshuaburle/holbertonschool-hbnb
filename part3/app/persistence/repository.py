fd1762ad52dc14744acba9230c6926096d16cd65
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-03-16 13:51:25 +0000

    add repository with the class SQLAlchemyRepository

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,93 +1,41 @@
 from abc import ABC, abstractmethod
 from app import db
 
-class Repository(ABC):
-    @abstractmethod
-    def add(self, obj):
-        pass
-
-    @abstractmethod
-    def get(self, obj_id):
-        pass
-
-    @abstractmethod
-    def get_all(self):
-        pass
-
-    @abstractmethod
-    def update(self, obj_id, data):
-        pass
-
-    @abstractmethod
-    def delete(self, obj_id):
-        pass
-
-    @abstractmethod
-    def get_by_attribute(self, attr_name, attr_value):
-        pass
-
-
-class InMemoryRepository(Repository):
-    def __init__(self):
-        self._storage = {}
-
-    def add(self, obj):
-        self._storage[obj.id] = obj
-
-    def get(self, obj_id):
-        return self._storage.get(obj_id)
-
-    def get_all(self):
-        return list(self._storage.values())
-
-    def update(self, obj_id, data):
-        obj = self.get(obj_id)
-        if obj:
-            obj.update(data)
-
-    def delete(self, obj_id):
-        if obj_id in self._storage:
-            del self._storage[obj_id]
-
-    def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
-
-class SQLAlchemyRepository(Repository):
+class SQLAlchemyRepository:
     def __init__(self, model):
         self.model = model
 
+    # Create
     def add(self, obj):
         db.session.add(obj)
         db.session.commit()
+        db.session.refresh(obj)
+        return obj
 
+    # Read
     def get(self, obj_id):
         return self.model.query.get(obj_id)
 
     def get_all(self):
         return self.model.query.all()
 
-    def update(self, obj_id, data):
+    # Update
+    def update(self, obj_id, data: dict):
         obj = self.get(obj_id)
-        if obj:
-            for key, value in data.items():
+        if obj is None:
+            return None
+        for key, value in data.items():
+            if hasattr(obj, key) and key not in ('id', 'created_at'):
                 setattr(obj, key, value)
-            db.session.commit()
+        db.session.commit()
+        db.session.refresh(obj)
+        return obj
 
+    # Delete
     def delete(self, obj_id):
         obj = self.get(obj_id)
-        if obj:
-            db.session.delete(obj)
-            db.session.commit()
-
-    def get_by_attribute(self, attr_name, attr_value):
-        return self.model.query.filter_by(**{attr_name: attr_value}).first()
-
-class UserRepository(SQLAlchemyRepository):
-    """User-specific repository with email lookup."""
-
-    def __init__(self):
-        from app.models.user import User
-        super().__init__(User)
-
-    def get_user_by_email(self, email: str):
-        return self.model.query.filter_by(email=email).first()
+        if obj is None:
+            return False
+        db.session.delete(obj)
+        db.session.commit()
+        return True
